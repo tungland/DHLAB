5759ed31ea923104fe2ea4de121865f0c30bdb06
Ingerid Dale <ingerid.dale@nb.no>
2022-03-28 17:10:40 +0200

    refactor: export modules in __init__.py
    Signed-off-by: Ingerid Dale <ingerid.dale@nb.no>

--- dhlab/__init__.py
+++ dhlab/__init__.py
@@ -1,3 +1,24 @@
+__docformat__ = "restructuredtext"
+
+__doc__ = """
+Digital Humanities lab
+---------------------------
+`dhlab` is a python library for accessing reduced representations of text and pictures at
+the National Library of Norway (NLN), *Nasjonalbiblioteket* (*NB*) in Norwegian.
+
+It is developed and maintained by `The Digital Humanities lab group <https://www.nb.no/dh-lab/>`_.
+"""
+__all__ = [
+    "nbtext",
+    "graph_networkx_louvain",
+    "token_map",
+    "nbpictures",
+    "nb_external_files",
+    "module_update",
+    "Corpus",
+    "Collocations",
+    "Concordance"
+]
 # legacy code
 from dhlab.text import nbtokenizer
 from dhlab.legacy import (
